4fe0e2583282ae23cb96c65bdcf525211cb311b7
chimchim270 <khimnurs27@gmail.com>
2025-10-16 00:49:07 +0700

    FINAL

--- frontend/tampil.php
+++ frontend/tampil.php
@@ -1,5 +1,5 @@
 <?php
-include "backend/koneksi.php";
+include "../backend/koneksi.php";
 $result = mysqli_query($conn, "SELECT * FROM anggota");
 ?>
 <!DOCTYPE html>
@@ -9,7 +9,7 @@ $result = mysqli_query($conn, "SELECT * FROM anggota");
 </head>
 <body>
     <h2> Daftar Siswa </h2>
-    <a href="backend/insert.php"> Tambah Data </a><br><br>
+    <a href="../backend/insert.php"> Tambah Data </a><br><br>
     <table border="1" cellpadding="5">
         <tr><th>ID</th><th>Nama</th><th>alamat</th><th>no_hp</th><th>Aksi</th></tr>
         <?php while ($row = mysqli_fetch_assoc($result)) { ?>
@@ -19,8 +19,8 @@ $result = mysqli_query($conn, "SELECT * FROM anggota");
             <td><?= $row['alamat']; ?></td>
             <td><?= $row['no_hp']; ?></td>
             <td>
-                <a href="backend/update.php?id=<?= $row['id']; ?>"> Edit </a> |
-                <a href="backend/delete.php?id=<?= $row['id']; ?>" onclick="return confirm('Yakin hapus?')"> Hapus </a> |
+                <a href="../backend/update.php?id=<?= $row['id']; ?>"> Edit </a> |
+                <a href="../backend/delete.php?id=<?= $row['id']; ?>" onclick="return confirm('Yakin hapus?')"> Hapus </a> |
             </td>
         </tr>
         <?php } ?>
